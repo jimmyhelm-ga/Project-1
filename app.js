1c7e34a0f126dce32089e806644a2d2c48b89980
Jimmy Helm <jimmyatthehelm@gmail.com>
2024-04-11 09:29:08 -0400

    initial structure

---
+++ app.js
@@ -0,0 +1,25 @@
+/*-------------------------------- Constants --------------------------------*/
+
+const button = document.querySelector(".button")
+
+
+
+/*-------------------------------- Variables --------------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+button.addEventListener("click", function() {
+    // This function will be executed when the button is clicked
+    alert("You get a cookie!");
+});
+
+
+/*-------------------------------- Functions --------------------------------*/
